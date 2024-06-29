3a465affb36032b461019b112f9ee190e3429d63
Jannatul Ferdous Nawrin <jannatulnawrin@gmail.com>
2024-06-29 21:32:02 +0600

    shadcn and project initial set up done

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>TODO APP</h1>
+      <Button>My button</Button>
+    </>
+  )
+}
+
+export default App

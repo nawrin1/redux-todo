2bb461afd2266277843f14d6f7b1cc1631c7b454
Jannatul Ferdous Nawrin <jannatulnawrin@gmail.com>
2024-07-01 21:39:51 +0600

    addtodo install done

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,24 +1,33 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
 
     return (
       <div>
         <div className="flex justify-between mb-5 ">
-            <button>Add to do</button>
-            <button>filter</button>
+            {/* <Button>Add to do</Button>
+            <Button>filter</Button> */}
+                    <AddTodoModal/>
+                    <TodoFilter />
             
         </div>
-        <div className="bg-primary-gradient w-full h-full rounded-xl  p-[5px] space-y-3">
-            {/* <TodoCard></TodoCard>
-            <TodoCard></TodoCard>
-            <TodoCard></TodoCard> */}
-                <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-md">
-          <p>There is no task pending</p>{' '}
+        <div className="bg-primary-gradient w-full h-full rounded-xl  p-[5px]">
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+        
+            <TodoCard  />
+            <TodoCard  />
+            <TodoCard  />
+
+        </div>
+                {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-md">
+          <p>There is no task pending</p>{' '} */}
         </div>
 
         </div>
-      </div>
+     
     );
   };
   
